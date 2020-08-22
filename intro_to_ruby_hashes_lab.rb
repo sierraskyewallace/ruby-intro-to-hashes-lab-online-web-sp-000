bfab62c6d525d0202897c57337a93b79716a3472
Sierra Wallace <sskwallace@gmail.com>
2020-08-22 23:07:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,10 +22,10 @@ def monopoly_with_second_tier
       :shortline => {}
     }, 
     :rent_in_dollars => {
-      :one_piece_owned => {25},
-      :two_pieces_owned => {50},
-      :three_pieces_owned => {100},
-      :four_pieces_owned => {200}
+      one_piece_owned: => 25,
+      two_pieces_owned: => 50,
+      three_pieces_owned: => 100,
+      four_pieces_owned: => 200
     }}}
 	end
 	end
